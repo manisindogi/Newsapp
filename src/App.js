683c6d83124d22f197841d7f070da566d242fb58
manisindogi <manisindogi1122@gmail.com>
2022-04-07 16:26:21 +0530

    NEWSAPP

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+export class App extends Component {
+  pageSize =9;
+  apikey=process.env.REACT_APP_NEWS_API
+  
+  render() {
+    return (
+      <div>
+        <Router>
+        <NavBar/>
+        
+        <Switch>
+          <Route exact path="/"> <News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/> </Route>
+          <Route exact path="/business"> <News apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"/> </Route>
+          <Route exact path="/entertainment"> <News apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"/> </Route>
+          <Route exact path="/general"> <News apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"/> </Route>
+          <Route exact path="/health"> <News apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"/> </Route>
+          <Route exact path="/science"> <News apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"/> </Route>
+          <Route exact path="/sports"> <News apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"/> </Route>
+          <Route exact path="/technology"> <News apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology"/> </Route>
+        </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
